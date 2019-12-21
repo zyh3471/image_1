c3808bb4f4b995ee4818b2f2fe774c1cf9b3d6c5
yehoom <zjzzhou@ucdavis.edu>
2019-12-21 01:56:05 +0000

    image1

---
+++ Image_1.rb
@@ -0,0 +1,18 @@
+class Image
+    def initialize(values)
+        @image = values
+
+    end 
+    def output_image
+        @image.each do |row|
+            puts row.join
+        end
+    end
+end
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+image.output_image
