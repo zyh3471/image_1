23090a159eeb9a35f76759eff3578d33195813fd
yehoom <zjzzhou@ucdavis.edu>
2019-12-22 00:57:57 +0000

    image2&3

--- image_test.rb
+++ image_test.rb
@@ -1,8 +1,8 @@
 class Image
     def initialize(values)
         @image = values
-
     end 
+
     def output_image
         @image.each do |row|
             puts row.join
@@ -10,21 +10,29 @@ class Image
     end
 
     def blur(n)
-        @image[values.index -1][values.column] = 1 unless values.index == 0
-        @image[values.index +1][values.column] = 1 unless valuese.index >= values.length-1
-
-        
+        n.times do
+            @image.each_with_index do |row, y|
+                row.each_with_index do |clo, x|
+                    if @image[y][x] == 1
+                        @image[y + 1][x] = 1 unless y == @image.length-1
+                        @image[y - 1][x] = 1 unless y == 0
+                        @image[y][x + 1] = 1 unless x == @image[y].length-1
+                        @image[y][x - 1] = 1 unless x == 0  
+                    end
+                end
+            end         
+        end
     end
-
-
 end
+
 image = Image.new([
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0]
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0]
 ])
-image.blur(2)
+image.blur(1)
 image.output_image
